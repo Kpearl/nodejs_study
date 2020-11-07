db6ace082f3ee1e604670efd069b47e7cdecb57a
Kpearl <kpearl115@gmail.com>
2020-11-07 16:08:16 +0900

    comment add

--- basic/objectLiteral.js
+++ basic/objectLiteral.js
@@ -18,9 +18,10 @@ console.log(oldObject.ES6); //Fantastic
 
 const newObject = {
   sayJS() {
+    //콜론(:), function 붙이지 않아도 됨
     console.log("JS");
   },
-  sayNode,
+  sayNode, //속성명과 변수명이 겹치는 경우 한번만 적음
   [es + 6]: "Fantastic",
 };
 newObject.sayNode(); //Node
