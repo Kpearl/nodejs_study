eaf45215008cdfa201635b7a8c5eb785185a7103
Kpearl <kpearl115@gmail.com>
2020-11-07 22:20:13 +0900

    helloWorld

--- 1. basic/repl.js
+++
@@ -1,3 +0,0 @@
-/*
-REPL(Read Eval Print Loop) : 읽기, 해석, 결과물 반환, 종료될때까지 반복
-*/
---
+++ 2. node/helloWorld.js
@@ -0,0 +1,14 @@
+/*
+REPL(Read Eval Print Loop) : 읽기, 해석, 결과물 반환, 종료될때까지 반복
+*/
+
+function helloWorld() {
+  console.log("Hello World");
+  helloNode();
+}
+
+function helloNode() {
+  console.log("Hello Node");
+}
+
+helloWorld();
