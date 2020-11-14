c1df0c6e2606d855710a8deb983059d0a5bceef8
Kpearl <keparl115@gmail.com>
2020-11-14 21:51:08 +0900

    server5 update

--- 3. http/server5.js
+++ 3. http/server5.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 
-const parseCookies = (cookie='') => {
-    cookie.split(';').map(v => v.split('=')).map(([k, ...vs]) => [k, vs.join('=')]).reduce((acc, [k,v]) => {
+const parseCookies = (cookie='') => cookie.split(';').map(v => v.split('=')).map(([k, ...vs]) => [k, vs.join('=')]).reduce((acc, [k,v]) => {
         acc[k.trim()] = decodeURIComponent(v);
         return acc;
     }, {});
@@ -16,20 +15,20 @@ const parseCookies = (cookie='') => {
         if(req.url.startsWith('/login')) {
             const {query} = url.parse(req.url);
             const {name} = qs.parse(query);
-            const exprise = new Date();
-            exprise.setMinutes(exprise.getMinutes() + 5);
+            const expires = new Date();
+            expires.setMinutes(expires.getMinutes() + 5);
             const randomInt = +new Date();
             session[randomInt] = {
-                name, exprise,
+                name, expires,
             };
             res.writeHead(302, {
                 Locatoin: '/',
-                'Set-Cookie' : `session=${randomInt}; Expires=${exprise.toGMTString()}; HttpOnly; Path=/`,
+                'Set-Cookie' : `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
             });
             res.end();
-        } else if(cookies.session && session[cookies.session].exprise > new Date()) {
+        } else if(cookies.session && session[cookies.session].expires > new Date()) {
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end(`${session[cookie.session].name}님 안녕하세요`);
+            res.end(`${session[cookies.session].name}님 안녕하세요`);
         } else {
             fs.readFile('./server4.html', (err, data) => {
                 if(err) {
@@ -38,6 +37,6 @@ const parseCookies = (cookie='') => {
                 res.end(data);
             });
         }
-    }).listen(8081, () => {
-        console.log('8080 포트에서 대기 중');
+    }).listen(8080, () => {
+        console.log('8080 포트에서 서버 대기중');
     });
