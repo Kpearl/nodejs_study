066930e1dbb463de59f4d05a6174afeee27135a0
Kpearl <kpearl115@gmail.com>
2020-11-18 20:30:51 +0900

    comment add

--- 4. express/learn-sequelize/models/comment.js
+++ 4. express/learn-sequelize/models/comment.js
@@ -1,1 +1,18 @@
-testtt
+const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('comment', {
+        comment: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        create_at : {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaltValue: DataTypes.NOW,
+        },
+    }, {
+        timestamps: false,
+    })
+}
