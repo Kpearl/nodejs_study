9b16d1e006a0814efd551e9b577950d18508bd21
Kpearl <kpearl115@gmail.com>
2020-11-23 13:26:24 +0900

    mongodb connect

--- 4. express/learn-mongoose/schemas/index.js
+++ 4. express/learn-mongoose/schemas/index.js
@@ -1,1 +1,27 @@
-testdsgfksdakhukfkwefhjkdafklhuadslhuqhoukaeshi
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    const connect = () => {
+        if(process.env.NODE_ENV !== 'production') {
+            mongoose.set('debug', true);
+        }
+        mongoose.connect('mongodb://localhost:27017/nodejs', 
+        (error) => {
+            if(error) {
+                console.log('디비 연결 에러', error);
+            } else {
+                console.log('몽고디비 연결 성공');
+            }
+        });
+    };
+    connect();
+    mongoose.connection.on('error', (error) => {
+        console.error('몽고디비 연결 에러', error);
+    });
+    mongoose.connection.on('disconnected', () => {
+        console.error('몽고디비 연결 끊김, 연결 재시도');
+        connect();
+    });
+    require('./user');
+    require('./comment');
+}
