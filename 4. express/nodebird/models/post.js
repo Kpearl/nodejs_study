4aa17291395e2a26eae51dd7f9d3bfa3e2b61405
Kpearl <kpearl115@gmail.com>
2020-11-24 17:17:55 +0900

    db update

---
+++ 4. express/nodebird/models/post.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize/types');
+const { sequelize } = require('.');
+
+module.exports = (sequelize, DataTypes) => {
+  sequelize.define(
+    'post',
+    {
+      content: {
+        type: DataTypes.STRING(140),
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING(200),
+        allowNull: true,
+      },
+    },
+    {
+      timestamp: true,
+      paranoid: true,
+    }
+  );
+};
