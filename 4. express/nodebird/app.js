697c115180be999cd2873307aa70d02fdfcaea41
Kpearl <kpearl115@gmail.com>
2020-11-24 17:04:53 +0900

    main update

--- 4. express/nodebird/app.js
+++ 4. express/nodebird/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
-require('dotenv').config();
 
 const pageRouter = require('./routes/page');
 
@@ -17,34 +16,36 @@ app.set('port', process.env.PORT || 8000);
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('process.env.COOKIE_SECRET'));
-app.use(session({
-    resave:false,
+app.use(
+  session({
+    resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-        httpOnly: true,
-        secure: false,
+      httpOnly: true,
+      secure: false,
     },
-}));
+  })
+);
 app.use(flash());
 
 app.use('/', pageRouter);
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트 대기중');
-})
+  console.log(app.get('port'), '번 포트 대기중');
+});
