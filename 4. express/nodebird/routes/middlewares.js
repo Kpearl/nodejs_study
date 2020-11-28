576df149321e73ef35c1d39cd5d97c7b6dfe6e46
Kpearl <kpearl115@gmail.com>
2020-11-28 20:00:38 +0900

    multer add

--- 4. express/nodebird/routes/middlewares.js
+++ 4. express/nodebird/routes/middlewares.js
@@ -1,6 +1,6 @@
 // 로그인 관련 접근을 제어하기 위하여 접근제어 미들웨어 추가
 exports.isLoggedIn = (req, res, next) => {
-    if(req.inAuthenticated()) {
+    if(req.isAuthenticated()) {
         next();
     } else {
         res.status(400).send('로그인 필요');
