96be6a0fa7851934c3510c1ee89eb3179cc255b7
Kpearl <kpearl115@gmail.com>
2020-12-01 22:36:39 +0900

    router update

--- 4. express/nodebird/routes/post.js
+++ 4. express/nodebird/routes/post.js
@@ -52,4 +52,27 @@ router.post('/', isLoggedIn, upload2.none(), async(req, res, next) => {
     next(error);
 }
 });
+
+router.get('/hashtag', async(req, res, next) => {
+    const query = req.query.hashtags;
+    if(!query) {
+        return res.redirect('/');
+    }
+    try {
+        const hashtag = await Hashtag.find({where: { title: query}});
+        let posts = [];
+        if(hashtag) {
+            posts = await hashtag.getPosts({include:[{model:User}]});
+        }
+        return res.render('main', {
+            title: `${query} | NodeBird`,
+            user: req.user,
+            twits: posts, 
+        });
+    } catch(error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
 module.exports = router;
