25e0239b3cbbdc86e721cbd72f4fa8edcc8ae0b5
Kpearl <kpearl115@gmail.com>
2022-06-13 17:01:23 +0900

    add chance, ramda

--- 7. TypeScript/ch02-2/src/index.ts
+++ 7. TypeScript/ch02-2/src/index.ts
@@ -1,10 +1,16 @@
-import IPerson from "./person/IPerson";
-import Person, {makePerson} from './person/Person'
+import IPerson from "./person/IPerson"
+import Person from './person/Person'
+import Chance from 'chance'
+import * as R from 'ramda'
 
-const testMakePerson = (): void => {
-    let jane: IPerson = makePerson('Jane')
-    let jack: IPerson = makePerson('Jack')
-    console.log(jane, jack)
-}
-
-testMakePerson()
+const chance = new Chance()
+let person: IPerson[] = R.range(0,2).map((n: number) => new Person(chance.name(), chance.age()))
+console.log(person)
+//
+// const testMakePerson = (): void => {
+//     let jane: IPerson = makePerson('Jane')
+//     let jack: IPerson = makePerson('Jack')
+//     console.log(jane, jack)
+// }
+//
+// testMakePerson()
